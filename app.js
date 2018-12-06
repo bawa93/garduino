d95fe07e520bd0c6a8c312f3f3efd434956d395b
Bawa <l_bawa@tss-hr420-dm19.fc.ca>
2018-12-06 14:19:11 -0500

    socket working properly sensor values are successfully passing to page and database

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-// board specific requirements
-// 
-// 
+var app = express();
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 // 
 var moistureSensor, tempSensor, lightSensor;
 var five = require("johnny-five"); // Load the node library that lets us talk JS to the Arduino
 var board = new five.Board(); // Connect to the Arduino using that library
 
-let httpServer = require('http').Server(app)
-let io = require('socket.io')(httpServer); 
+// let httpServer = require('http').Server(app)
+// let io = require('socket.io')(httpServer); 
 var mysql = require('mysql')
-
-
 
 // database logic start
 //  
@@ -32,95 +32,17 @@ var connection = mysql.createConnection({
     database: 'sql3268620'
 });
 
-
 connection.connect(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
     }
 
-    console.log('connected as id ' + connection.threadId);
+    console.log('database connected as id ' + connection.threadId);
 });
 
 
 //connection.end()
-
-
-// databse logic end
-// socket logic start
-
-io.on('connection', function(socket) {
-
-
-});
-
-
-// socket logic end
-// 
-
-
-
-
-// board ready code here
-// 
-// 
-// 
-// 
-board.on("ready", function() {
-
-    // setup moisture sensor to correct pin
-    // setup temperature sensor LM35
-    tempSensor = new five.Thermometer({
-        controller: 'LM35',
-        pin: 'A1',
-        freq: 250
-    });
-
-    // setup moisture sensor to correct pin
-    moistureSensor = new five.Sensor({
-        pin: 'A2',
-        freq: 250
-    });
-
-    // setup light sensor to correct pin
-    lightSensor = new five.Sensor({
-        pin: 'A3',
-        freq: 250
-    });
-});
-
-//
-
-// board ready code end
-//  
-
-// process sensor data
-
-setInterval(function() {
-    emitChartData(io, tempSensor, lightSensor, moistureSensor)
-}, 1000)
-
-// save measurement to rethinkdb on each interval
-setInterval(function() {
-    saveRecordings(connection, tempSensor, lightSensor, moistureSensor)
-}, 10000)
-
-
-
-// process sensor data end
-
-var app = express();
-
-
-// functions
-// 
-// 
-// 
-// 
-// 
-function emitChartData(io, tempSensor, lightSensor, moistureSensor) {
-
-}
 
 function saveRecordings(connection, tempSensor, lightSensor, moistureSensor) {
 
@@ -130,14 +52,99 @@ function saveRecordings(connection, tempSensor, lightSensor, moistureSensor) {
     var moistureVal = optimizeMoisture(moistureSensor);
 
     console.log(moistureVal);
-    // console.log(tempVal);
+    console.log(lightVal);
 
-    connection.query('INSERT INTO `readings` (`id`, `plant_id`, `moisture`, `temperature`, `light`, `updated_at`) VALUES (NULL, ' + `"1", "${moistureVal}", "455", "333", CURRENT_TIMESTAMP);`, function(err, rows, fields) {
+    console.log(tempVal);
+
+    connection.query('INSERT INTO `readings` (`id`, `plant_id`, `moisture`, `temperature`, `light`, `updated_at`) VALUES (NULL, ' + `"1", "${moistureVal}", "${tempVal}", "${lightVal}", CURRENT_TIMESTAMP);`, function(err, rows, fields) {
         if (err) throw err
-
-        console.log('item was inserted: ', rows[0])
+        var last_inserted_id = rows.insertId;
+        return last_inserted_id;
+        console.log('Last inserted item id : ', rows.insertId)
     });
 
+}
+
+// databse logic end
+// socket logic start
+
+
+// socket logic end
+// 
+
+// board ready code here
+// 
+
+board.on("ready", function() {
+
+    // setup moisture sensor to correct pin
+    // setup temperature sensor LM35
+    tempSensor = new five.Thermometer({
+        controller: 'LM35',
+        pin: 'A2',
+        freq: 250
+    });
+
+    // setup moisture sensor to correct pin
+    moistureSensor = new five.Sensor({
+        pin: 'A3',
+        freq: 250
+    });
+
+    // setup light sensor to correct pin
+    lightSensor = new five.Sensor({
+        pin: 'A0',
+        freq: 250
+    });
+});
+  
+//
+
+// board ready code end
+
+// process sensor data
+setTimeout(function() {
+    setInterval(function() {
+        emitChartData(io, tempSensor, lightSensor, moistureSensor)
+    }, 1000);
+},10000);
+
+// save measurement to rethinkdb on each interval
+setInterval(function() {
+    saveRecordings(connection, tempSensor, lightSensor, moistureSensor)
+}, 10000)
+
+
+// process sensor data end
+
+//    io.on('connection', function (socket) {
+//     console.log('io connection established');
+//     console.log('socket connected as ' + socket.id)
+
+//     // emit usersCount on new connection
+//     emitUsersCount(io)
+
+//     // emit usersCount when connection is closed
+//     socket.on('disconnect', function () {
+//       emitUsersCount(io)
+//         });
+
+// });
+
+// functions
+// 
+function emitChartData(io, tempSensor, lightSensor, moistureSensor) {
+    /* GET home page. */
+    var tempVal = optimizeTemp(tempSensor);
+    var lightVal = optimizeLight(lightSensor);
+    var moistureVal = optimizeMoisture(moistureSensor);
+
+    // console.log(moistureVal.value);
+    io.emit("moisture", moistureVal);
+    io.emit("temperature", tempVal);
+    io.emit("light", lightVal);
+
+  
 }
 
 function optimizeTemp(tempSensor) {
@@ -154,8 +161,34 @@ function optimizeMoisture(moisture) {
     return Math.round(moisture.value / 1023 * 100)
 }
 
+// get random int in range of min and max --> was used to mock out data
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+  
+  // get all measurements of certain type
+  function getAllMeasurements(plant_id) {
 
-
+    connection.query(`SELECT * from readings where plant_id=` + plant_id, function (err, rows, fields) {
+        if (err) throw err
+      return rows;
+      });
+  }
+  
+  // get all temperature measurements
+  function getAllTemperatureMeasurements(plant_id) {
+    return getAllMeasurementsOfCertainType('temperature', plant_id)
+  }
+  
+  // get all temperature measurements
+  function getAllLightMeasurements(plant_id) {
+    return getAllMeasurementsOfCertainType('light', plant_id)
+  }
+  
+  // get all temperature measurements
+  function getAllMoistureMeasurements(plant_id) {
+    return getAllMeasurementsOfCertainType('moisture', plant_id)
+  }
 
 // functions end
 
@@ -188,4 +221,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+  
+module.exports = {app: app, server: server}; 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
 });
+
+router.get('/temperature', function (req, res, next) {
+  res.render('temperature')
+})
+
+router.get('/light', function (req, res, next) {
+  res.render('light')
+})
+
+router.get('/moisture', function (req, res, next) {
+  res.render('moisture')
+})
+
+
+// Routes for data
+router.get('/api/temps', function (req, res, next) {
+  getAllTemperatureMeasurements(function (err, measurements) {
+    if (err) { console.log(err) }
+
+    res.write(JSON.stringify(measurements))
+    res.end()
+  })
+})
+
+router.get('/api/light', function (req, res, next) {
+  getAllLightMeasurements(function (err, measurements) {
+    if (err) { console.log(err) }
+
+    res.write(JSON.stringify(measurements))
+    res.end()
+  })
+})
+
+router.get('/api/moisture', function (req, res, next) {
+  getAllMoistureMeasurements(function (err, measurements) {
+    if (err) { console.log(err) }
+
+    res.write(JSON.stringify(measurements))
+    res.end()
+  })
+})
+
+
 
 module.exports = router;
